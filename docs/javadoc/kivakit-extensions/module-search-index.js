81bf6923d39583441824b8b9a8ee7d663e4bb5fc
Jonathan Locke <jonathanl@telenav.com>
2021-04-27 08:42:30 -0700

    📝 Updated documentation for 0.9.5-alpha-SNAPSHOT

--- docs/javadoc/kivakit-extensions/module-search-index.js
+++ docs/javadoc/kivakit-extensions/module-search-index.js
@@ -1,1 +1,1 @@
-moduleSearchIndex = [{"l":"kivakit.data.compression"},{"l":"kivakit.data.formats.csv"},{"l":"kivakit.data.formats.library"},{"l":"kivakit.math"},{"l":"kivakit.ui.swing"}]
+moduleSearchIndex = [{"l":"kivakit.data.compression"},{"l":"kivakit.data.formats.csv"},{"l":"kivakit.data.formats.library"},{"l":"kivakit.math"},{"l":"kivakit.ui.desktop"}]
